102c09c9dac0f6954f5c3cac5e41b94208e16045
Rihanna <ms.rihanna.wright06@gmail.com>
2026-01-15 16:58:01 +0000

    Fixed assertions in AboutClassMethods, AboutClasses, AboutDiceProject, AboutInheritance, AboutModules, AboutOpenClasses and AboutScope.

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -1,10 +1,14 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
-#
-# class DiceSet
-#   code ...
-# end
+# generate a list of random numbers between 1 and 6.
+class DiceSet
+  attr_reader :values
+
+  def roll(number_of_dice)
+    @values = Array.new(number_of_dice) { rand(1..6) }
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
@@ -48,6 +52,7 @@ class AboutDiceProject < Neo::Koan
     # If the rolls are random, then it is possible (although not
     # likely) that two consecutive rolls are equal.  What would be a
     # better way to test this?
+    # To check the object_id of the values array to ensure that a new array is created on each roll.
   end
 
   def test_you_can_roll_different_numbers_of_dice
